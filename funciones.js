a7e95e9985f39429dc7d939cf0188ddffd377fe9
f3lix1110 <felixhdz1110@gmail.com>
2021-12-20 17:24:42 -0600

    Primeros ejercicios de algoritmos

---
+++ funciones.js
@@ -0,0 +1,54 @@
+function impresion(){   
+    console.log("Felix"); //Impresion en java
+}
+
+//impresion(); //invocacion de la funcion
+
+// comentarios multilinea /* */
+// con ctrl+k+c cada linea  &  descomentar 
+// con ctrl+k+u
+
+/*
+function persona(nombre, apellido, edad, cuadrado){
+
+    cuadrado=edad*edad; //edad al cuadrado
+    return ('Bienvenido: '+nombre+' '+apellido+' y tu edad es: '+edad+' y tu edad al cuadrado es: '+cuadrado);
+}
+console.log(persona('Felix','Hernandez',23));
+console.log(persona('Guillermo','Chang',34));
+console.log(persona('Ernesto','Diaz',29));
+
+
+//Ejemplo 6:
+//Función IIFEs (funciones invocadas inmediatamente)
+(function(apellido){
+    console.log('Felix '+apellido);
+  })('Hernandez');
+*/
+
+//Declaración de valores
+var numero1 = 10;
+var numero2 = 3;
+
+//Operadores aritméticos básicos
+valor1 = numero1 + numero2;
+valor2 = numero1 * numero2;
+valor3 = numero1 - numero2;
+valor4 = numero1 / numero2;
+valor5 = numero1 % numero2;
+numero1++;
+numero2--;
+
+console.log(valor1);
+console.log(valor2);
+console.log(valor3);
+console.log(valor4);
+console.log(valor5);
+console.log(numero1);
+console.log(numero2);
+
+
+
+
+
+
---
+++ retorno.js
@@ -0,0 +1,7 @@
+function cuadrado(a){ //retornamos la variable "a" que utilizaremos dentro de la funcion
+    return a*a;
+}
+
+console.log(cuadrado(2));
+console.log(cuadrado(3));
+console.log(cuadrado(4));
