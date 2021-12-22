7ce77446026fd91a3d5063596dfb90f5e1367f89
f3lix1110 <felixhdz1110@gmail.com>
2021-12-22 00:17:26 -0600

    Ejemplo de condicional

---
+++ ejemplo.js
@@ -0,0 +1,17 @@
+let edad = prompt("Cual es tu edad");
+
+if (edad>=18) {
+  alert("Eres mayor de edad");}
+  else{
+      alert("Eres menor de edad")
+  }
+
+let number1 = parseInt(prompt("Introduce un numero"));
+let number2 = parseInt(prompt("Introduce otro numero"));
+
+functions aritmetica(number1, number2){
+	suma=number1+number2;
+	alert("suma es: "+suma);
+}
+
+aritmetica();
